ab75196fd0c9ee5122c7754e5205b8880907ad81
Gregory Brutsche <gregory@mail.brutsches.local>
2023-11-24 19:18:54 -0500

    Misc changes/updates.

--- DispenserPatch.cs
+++ DispenserPatch.cs
@@ -6,20 +6,16 @@ namespace Dispenser_Pulls_Only_One {
     [HarmonyPatch]
     [UsedImplicitly]
     public static class DispenserPatch {
-        private static readonly MethodInfo DISPENSER_MOVE_TO = typeof(Dispenser).GetMethod("MoveTo", BindingFlags.NonPublic | BindingFlags.Instance);
-
         [HarmonyTargetMethod]
         [UsedImplicitly]
         public static MethodBase TargetMethod() {
-            return typeof(Dispenser).GetMethod("LoadSlot", BindingFlags.NonPublic | BindingFlags.Instance);
+            return typeof(Dispenser).GetMethod(nameof(Dispenser.LoadSlot), BindingFlags.NonPublic | BindingFlags.Instance);
         }
 
         private static void LoadSlot(ref Dispenser __instance, ref Dispenser.Element slot, ItemDefinition targetItem, ref OnlineCargoConnector m_connector) {
             if (slot.Item.Item != targetItem && !slot.Animation.IsAnimating) {
                 if (slot.Item.IsValid) {
-                    var args = new object[] {slot, m_connector.OnlineCargo, Dispenser.State.Unload, 0};
-                    DISPENSER_MOVE_TO.Invoke(__instance, args);
-                    slot = (Dispenser.Element) args[0];
+                    __instance.MoveTo(ref slot, m_connector.OnlineCargo, Dispenser.State.Unload);
                     return;
                 }
                 if (targetItem != null) {
